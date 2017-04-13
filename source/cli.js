af67841aae651fd88150031aed33713e9de01f7d
Quildreen Motta <quildreen@gmail.com>
2017-04-12 23:13:47 -0300

    Adds support for providing plugins

--- source/cli.js
+++ source/cli.js
@@ -14,16 +14,31 @@ const VM = require('./vm');
 const runtime = require('./runtime');
 const package = require('../package.json');
 
+const collect = (value, memo) => {
+  memo.push(value);
+  return memo;
+};
+
+const toModuleId = (module) => {
+  if (/^\./.test(module)) {
+    return path.resolve(process.cwd(), module);
+  } else {
+    return module;
+  }
+};
+
+
 program.version(package.version);
 
-
-program.command('run [file] [pipeline]')
+program.command('run <file> <pipeline>')
   .description(`Runs the provided pipeline to completion.`)
+  .option('-p, --plugin [module]', 'The module ID of a Furipota plugin', collect, [])
   .action(async (definitions, pipeline, options) => {
     try {
       const file = path.resolve(process.cwd(), definitions);
       const vm = new VM(path.dirname(file));
       vm.plugin(runtime);
+      options.plugin.forEach(plugin => vm.plugin(require(toModuleId(plugin))));
       await vm.import(file);
       const stream = vm.global.get(pipeline).invoke(vm);
       stream.subscribe({
@@ -41,3 +56,7 @@ program.command('run [file] [pipeline]')
   });
 
 program.parse(process.argv);
+
+if (process.argv.length < 3) {
+  program.help();
+}
